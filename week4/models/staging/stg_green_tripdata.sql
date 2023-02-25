ae5b4d086b0263dc5f86524576f693a57a46562e
Abhinav Gadgil <abhigadgil15@gmail.com>
2023-02-25 10:49:11 +0000

    error week4

--- week4/models/staging/stg_green_tripdata.sql
+++ week4/models/staging/stg_green_tripdata.sql
@@ -1,7 +1,14 @@
 {{ config(materialized='view') }}
 
-
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
+)
 select
+    -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
@@ -24,19 +31,17 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
+    -- cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
+from tripdata
+where rn = 1
 
 
-
-from {{ source('staging','green_tripdata') }}
-where vendorid is not null
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=false) %}
 
   limit 100
-
-{% endif %}
